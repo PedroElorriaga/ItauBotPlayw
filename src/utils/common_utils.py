ad78d5fd84009371471e54c35c69dc63d69dccfa
PedroElorriaga <pedrohes2002@outlook.com>
2025-08-22 10:37:57 -0300

    feat: change data type util

--- src/utils/common_utils.py
+++ src/utils/common_utils.py
@@ -1,4 +1,5 @@
 import json
+from typing import List
 
 
 def read_json_file(file: str):
@@ -6,3 +7,10 @@ def read_json_file(file: str):
         data = json.load(json_file)
 
     return data
+
+
+def tuple_list_to_str_list(data: List[tuple]):
+    if len(data) > 0:
+        string_list = [f'{arg[0]} - {arg[1]}' for arg in data]
+
+        return string_list
