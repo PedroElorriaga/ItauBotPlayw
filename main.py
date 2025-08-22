375e7d57e9e93fdd6403bc46f249cd36a1722a68
PedroElorriaga <pedrohes2002@outlook.com>
2025-08-22 18:07:17 -0300

    feat: change account logic handle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.pages.login_page import LoginPage
 from src.pages.companies_page import CompaniesPage
 from src.config.settings import PlaywrightsConfigs, ItauConfigs
+from src.utils.common_utils import tuple_list_to_str_list, SystemMessages
 from src.models.duckdb.connection import DuckConnection
 import asyncio
 
@@ -10,6 +11,7 @@ async def connect_duckdb():
         from src.utils.errors_utils import ConnectionFailedDuckDb
         duckdb = DuckConnection(
             'docs/database/companies.duckdb')
+        SystemMessages().success('Feito conexão com a base de dados...')
         return duckdb
     except ConnectionFailedDuckDb.IOException:
         raise ConnectionFailedDuckDb(
@@ -20,8 +22,8 @@ async def do_itau_tasks():
     async with PlaywrightsConfigs() as context:
         duckdb_connection = await connect_duckdb()
 
-        companies_from_progress_table = duckdb_connection.search_companies_status_pending()
-        print(companies_from_progress_table)
+        companies_from_progress_table = tuple_list_to_str_list(
+            duckdb_connection.search_companies_status_pending())
         page = await context.new_page()
         companies_to_execute = companies_from_progress_table if len(
             companies_from_progress_table) > 0 else ItauConfigs.COMPANIES_TO_EXECUTE
@@ -32,10 +34,19 @@ async def do_itau_tasks():
         companies_itau = CompaniesPage(
             page, companies_to_execute=companies_to_execute)
         accounts = await companies_itau.get_accounts()
-        print(accounts)
-
         duckdb_connection.insert_companies_if_not_exists(accounts)
 
+        for account in accounts:
+            SystemMessages().log(
+                f'Trocando conta para {account["name"]}...')
+
+            if account['index'] == 0:
+                SystemMessages().log(
+                    f'Conta trocada para {account["name"]} - {account["cnpj"]}')
+            else:
+                await companies_itau.change_account(account)
+
+            await companies_itau.goto_download_company_page()
 
 if __name__ == '__main__':
     asyncio.run(do_itau_tasks())
