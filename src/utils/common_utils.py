3e94655aed6a3ddc5b6d1fd2d4e916b6efb618a2
PedroElorriaga <pedrohes2002@outlook.com>
2025-08-22 18:05:17 -0300

    feat: system messages utils

--- src/utils/common_utils.py
+++ src/utils/common_utils.py
@@ -1,8 +1,8 @@
-import json
 from typing import List
 
 
 def read_json_file(file: str):
+    import json
     with open(file + '.json', 'r') as json_file:
         data = json.load(json_file)
 
@@ -14,3 +14,15 @@ def tuple_list_to_str_list(data: List[tuple]):
         string_list = [f'{arg[0]} - {arg[1]}' for arg in data]
 
         return string_list
+
+    return []
+
+
+class SystemMessages:
+    from colorama import Fore
+
+    def log(self, message: str):
+        print(self.Fore.MAGENTA + message + self.Fore.RESET)
+
+    def success(self, message: str):
+        print(self.Fore.GREEN + message + self.Fore.RESET)
