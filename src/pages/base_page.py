81c10822099aa30d94c9fe0f09be6fab560a7a44
PedroElorriaga <pedrohes2002@outlook.com>
2025-09-02 00:32:59 -0300

    feat: iframe included in constructor method

--- src/pages/base_page.py
+++ src/pages/base_page.py
@@ -4,6 +4,7 @@ from playwright.async_api import BrowserContext
 class BasePage:
     def __init__(self, page: BrowserContext, base_url: str = None):
         self.page = page
+        self.iframe_page = None
         self.base_url = base_url
 
     async def open_url(self):
