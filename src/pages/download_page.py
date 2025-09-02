c26f4117e6a6c91a3d0f3aecdba5187c2134abe4
PedroElorriaga <pedrohes2002@outlook.com>
2025-09-02 20:20:14 -0300

    fix: Itau Bot Support error handle

--- src/pages/download_page.py
+++ src/pages/download_page.py
@@ -12,18 +12,27 @@ class DownloadPage(BasePage):
 
     async def search_payments(self):
         self.iframe_page = self.page.locator('iframe.iframe-nf2')
+        chat_itau_element = self.iframe_page.content_frame.locator(
+            '#floatAjudaMovel')
+        await chat_itau_element.evaluate("""
+            () => {
+                const btn = document.querySelector('#floatAjudaMovel');
+                if (btn) btn.style.pointerEvents = 'none';
+            }
+        """)
+        await self.page.wait_for_timeout(30000)
         await self.iframe_page.content_frame.get_by_role("combobox", name="data").click()
         await self.iframe_page.content_frame.get_by_text("Outra data ou período").click()
         await self.__insert_date_interval()
         await self.iframe_page.content_frame.get_by_role("button", name="buscar").click()
         try:
+            # VERIFICA SE EXISTE ALGUM COMPROVANTE, SE NÃO, LANÇA EXCEPTION
             locator = self.iframe_page.content_frame.get_by_text(
                 "nenhum pagamento encontrado")
             await locator.wait_for(timeout=2500)
         except LocatorTimeoutPlaywright.TimeoutError:
+            # VERIFICA SE EXISTE O TUTORIAL, SE NÃO, LANÇA EXCEPTION
             await self.iframe_page.content_frame.get_by_role("button", name="Fechar tutorial").click(timeout=2500)
-
-        print('labubu')
 
     async def __insert_date_interval(self):
         await self.iframe_page.content_frame.get_by_role("textbox", name="data inicial").click()
