74b25faa20b8bfa56892e61aaba5ed0107350a80
PedroElorriaga <pedrohes2002@outlook.com>
2025-09-12 20:13:57 -0300

    feat: new retry logic handle

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from src.pages.itau_page.login_page import LoginPage
+from src.pages.itau_page.login_page import LoginPageItau
 from src.pages.itau_page.companies_page import CompaniesPage
 from src.pages.itau_page.download_page import DownloadPage
-from src.pages.netsuite_page.login_page import LoginPage
+from src.pages.netsuite_page.login_page import LoginPageNetsuite
 from src.config.settings import PlaywrightsConfigs, ItauConfigs, NetsuiteConfigs
-from src.utils.common_utils import tuple_list_to_str_list, SystemMessages
+from src.utils.common_utils import tuple_list_to_str_list, SystemMessages, RetryExecuter
 from src.models.duckdb.connection import DuckConnection
 import asyncio
 
@@ -22,13 +22,15 @@ async def connect_duckdb():
 
 async def do_itau_tasks():
     async with PlaywrightsConfigs() as context:
+        retry = RetryExecuter()
         duckdb_connection = await connect_duckdb()
 
         companies_from_progress_table = tuple_list_to_str_list(
             duckdb_connection.search_all_companies())
 
         companies_pending_status = [
-            company.replace(' - pending', '') for company in companies_from_progress_table if 'pending' in company]
+            company.replace(' - pending', '').replace(' - error', '')
+            for company in companies_from_progress_table if 'pending' in company or 'error' in company]
 
         if len([company for company in companies_from_progress_table if 'done' in company]) > 0:
             if len(companies_pending_status) == 0:
@@ -38,7 +40,7 @@ async def do_itau_tasks():
             companies_from_progress_table) > 0 else ItauConfigs.COMPANIES_TO_EXECUTE
 
         page = await context.new_page()
-        login_itau = LoginPage(
+        login_itau = LoginPageItau(
             page, ItauConfigs.OPERATOR_ITAU, 'https://www.itau.com.br/itaubba-pt',)
         await login_itau.goto_login()
 
@@ -48,41 +50,35 @@ async def do_itau_tasks():
         duckdb_connection.insert_companies_if_not_exists(accounts)
 
         for account in accounts:
-            # TODO FAZER TENTATIVAS PARA CADA EMPRESA
-            SystemMessages().log(
-                f'Trocando conta para {account["name"]} - {account["number"]}...')
-
-            if account['index'] == 0:
+            try:
                 SystemMessages().log(
-                    f'Conta trocada para {account["name"]} - {account["cnpj"]} - {account["number"]}')
-            else:
-                await companies_itau.change_account(account)
+                    f'Trocando conta para {account["name"]} - {account["number"]}...')
 
-            await companies_itau.goto_download_company_page()
-            download_itau = DownloadPage(
-                page, date_begin=ItauConfigs.DATE_BEGIN, date_end=ItauConfigs.DATE_END)
-            await download_itau.search_payments()
+                if account['index'] == 0:
+                    SystemMessages().log(
+                        f'Conta trocada para {account["name"]} - {account["cnpj"]} - {account["number"]}')
+                else:
+                    await retry.run(companies_itau.change_account, account)
 
-            duckdb_connection.update_company_status(account, 'done')
+                await retry.run(companies_itau.goto_download_company_page)
+                download_itau = DownloadPage(
+                    page, date_begin=ItauConfigs.DATE_BEGIN, date_end=ItauConfigs.DATE_END)
+                await retry.run(download_itau.search_payments)
 
-        SystemMessages().success('Tarefas do Itau foram executadas com sucesso!')
+                duckdb_connection.update_company_status(account, 'done')
+            except Exception as err:
+                duckdb_connection.update_company_status(account, 'error')
+
+            SystemMessages().success('Tarefas do Itau foram executadas com sucesso!')
 
 
 async def do_netsuite_tasks():
     async with PlaywrightsConfigs() as context:
         page = await context.new_page()
-        login_netsuite = LoginPage(
+        login_netsuite = LoginPageNetsuite(
             page, 'https://system.netsuite.com/pages/customerlogin.jsp')
         await login_netsuite.goto_login()
 
 if __name__ == '__main__':
-    tentativas_mvp = 3
-
-    while tentativas_mvp > 0:
-        try:
-            asyncio.run(do_itau_tasks())
-            asyncio.run(do_netsuite_tasks())
-            break
-        except Exception as err:
-            SystemMessages().error(err)
-            tentativas_mvp -= 1
+    asyncio.run(do_itau_tasks())
+    asyncio.run(do_netsuite_tasks())
--- src/utils/common_utils.py
+++ src/utils/common_utils.py
@@ -1,7 +1,8 @@
 from typing import List
+from typing import Callable, Awaitable, Any
 
 
-def read_json_file(file: str):
+def read_json_file(file: str) -> Any:
     import json
     with open(file + '.json', 'r') as json_file:
         data = json.load(json_file)
@@ -9,7 +10,7 @@ def read_json_file(file: str):
     return data
 
 
-def tuple_list_to_str_list(data: List[tuple]):
+def tuple_list_to_str_list(data: List[tuple]) -> List[Any]:
     if len(data) > 0:
         string_list = [
             f'{arg[0]} - {arg[1]} - {arg[2]} - {arg[3]}' for arg in data]
@@ -22,17 +23,17 @@ def tuple_list_to_str_list(data: List[tuple]):
 class SystemMessages:
     from colorama import Fore
 
-    def log(self, message: str):
+    def log(self, message: str) -> Any:
         print(self.Fore.MAGENTA + message + self.Fore.RESET)
 
-    def success(self, message: str):
+    def success(self, message: str) -> Any:
         print(self.Fore.GREEN + message + self.Fore.RESET)
 
-    def error(self, message: str):
+    def error(self, message: str) -> Any:
         print(self.Fore.RED + message + self.Fore.RESET)
 
 
-def access_download_dir(filename: str):
+def access_download_dir(filename: str) -> str:
     import os
     path_dir = os.path.join(os.getcwd(), 'docs/downloads')
     save_path = os.path.join(path_dir, filename)
@@ -40,8 +41,27 @@ def access_download_dir(filename: str):
     return save_path
 
 
-def check_if_data_dont_have_special_character(data: str):
+def check_if_data_dont_have_special_character(data: str) -> str:
     import re
     check = re.sub(r'[<>:"/\\|?*]', "_", data)
 
     return check
+
+
+class RetryExecuter:
+    def __init__(self):
+        self.attempts = 3
+        self.exception = Exception
+
+    async def run(self, func: Callable[..., Awaitable[Any]], *args, **kwargs) -> Any:
+        from src.utils.common_utils import SystemMessages
+
+        attempts = self.attempts
+        while attempts > 0:
+            try:
+                return await func(*args, **kwargs)
+            except self.exception as err:
+                attempts -= 1
+                if attempts == 0:
+                    SystemMessages().error(f'RetryExecuter -> {str(err)}')
+                    raise err
