74867f3381a8132217ed9b6de9b9f9e73573a3f9
PedroElorriaga <pedrohes2002@outlook.com>
2025-09-19 13:40:15 -0300

    fix: started use SystemMessages and use settings for password

--- src/pages/itau_page/login_page.py
+++ src/pages/itau_page/login_page.py
@@ -1,17 +1,19 @@
 from src.pages.base_page import BasePage
-from src.config.settings import ItauConfigs
+from src.utils.common_utils import SystemMessages
 
 
 class LoginPageItau(BasePage):
-    def __init__(self, page, operator_password: str, base_url: str = None):
+    def __init__(self, page, operator_password: str, itau_password: str, base_url: str = None):
         super().__init__(page, base_url)
         self.__operator_password = operator_password
+        self.__itau_password = itau_password
 
     async def goto_login(self):
         await self.open_url()
         await self.page.get_by_role("button", name="Mais acessos").click()
 
         await self.__make_login_using_operator()
+        SystemMessages().success('Login itau efetuado com sucesso!')
 
     async def __make_login_using_operator(self):
         await self.page.get_by_label("Opções de acesso").click()
@@ -24,7 +26,7 @@ class LoginPageItau(BasePage):
         await self.__enter_virtual_keyboard_password()
 
     async def __enter_virtual_keyboard_password(self):
-        password_list = [number for number in ItauConfigs.PASSWORD_ITAU]
+        password_list = [number for number in self.__itau_password]
         await self.page.wait_for_selector('.teclado.clearfix')
         await self.page.wait_for_timeout(1000)
         keyboards_elements = await self.page.locator('#campoTeclado').all()
