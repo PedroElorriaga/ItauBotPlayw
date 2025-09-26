df3752d6e7dc772f121220c02337aaaed7daaf7a
Pedro Santos <pedro.santos2@tmf-group.com>
2025-09-25 21:34:21 -0300

    refactor: new functions parameter added

--- src/pages/netsuite_page/upload_page.py
+++ src/pages/netsuite_page/upload_page.py
@@ -1,5 +1,5 @@
 from src.pages.base_page import BasePage
-from src.utils.common_utils import access_download_dir, SystemMessages
+from src.utils.common_utils import access_dir, SystemMessages
 
 
 class UploadPage(BasePage):
@@ -7,23 +7,24 @@ class UploadPage(BasePage):
         super().__init__(page, base_url)
 
     async def upload_file(self, file: str):
-        SystemMessages().log('Enviando comprovantes...')
-
         total_element_text = await self.page.locator('#uir-total-count').inner_text()
         if total_element_text.split(':')[1].strip() == '0':
             SystemMessages().error('Nenhum comprovante encontrado')
             return
 
         await self.page.get_by_role("link", name="Fatura #").click()
+        await self.page.wait_for_timeout(2000)
         await self.page.locator("#main_form").get_by_role("listitem").filter(has_text="arquivo Nova nota Status").get_by_role("link").click()
 
         # PAGINA NO CONTEXTO DO POPUP
         async with self.page.expect_popup() as pageup:
             await self.page.get_by_role("button", name="arquivo").click()
 
+        SystemMessages().log('Enviando comprovantes...')
+
         page_popup = await pageup.value
         await page_popup.get_by_role("textbox", name="Nome do arquivo").fill(file)
-        await page_popup.set_input_files('input[type="file"]', access_download_dir(file))
+        await page_popup.set_input_files('input[type="file"]', access_dir('docs/downloads', file))
         await page_popup.wait_for_timeout(1500)
         await page_popup.get_by_role("button", name="Salvar").click()
         await page_popup.close()
