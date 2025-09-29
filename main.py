1bc1dafa36621e9d6091426b6f02158f0f2d6f0b
Pedro Santos <pedro.santos2@tmf-group.com>
2025-09-29 17:00:53 -0300

    feat: new feat for progress repository and tqdm log handle

--- main.py
+++ main.py
@@ -6,30 +6,18 @@ from src.pages.netsuite_page.search_page import SearchPage
 from src.pages.netsuite_page.upload_page import UploadPage
 from src.config.settings import PlaywrightsConfigs, ItauConfigs, NetsuiteConfigs
 from src.utils.common_utils import *
-from src.models.duckdb.connection import DuckConnection
+from src.services.system_messages import SystemMessages
+from src.services.retry_executer import RetryExecuter
+from src.models.duckdb.settings.connection import DuckConnection
+from src.models.duckdb.repository.progress_repository import ProgressRepository
+from tqdm.asyncio import tqdm
 import asyncio
-
-
-async def connect_duckdb():
-    try:
-        from src.utils.errors_utils import ConnectionFailedDuckDb
-        duckdb = DuckConnection(access_dir(
-            'docs\database', 'companies.duckdb'))
-        SystemMessages().success('Feito conexão com a base de dados...')
-        return duckdb
-    except ConnectionFailedDuckDb.IOException:
-        SystemMessages().error(
-            'Ocorreu algum erro ao tentar se conectar com a base de dados :(')
-        raise ConnectionFailedDuckDb(
-            'Failed to connect in DuckDB engine')
 
 
 async def do_itau_tasks():
     async with PlaywrightsConfigs() as context:
-        duckdb_connection = await connect_duckdb()
-
         companies_from_progress_table = tuple_list_to_str_list(
-            duckdb_connection.search_all_companies())
+            progress_repository.search_all_companies())
 
         companies_pending_status = [
             company.replace(' - pending', '').replace(' - error', '')
@@ -37,7 +25,7 @@ async def do_itau_tasks():
 
         if len([company for company in companies_from_progress_table if 'done' in company]) > 0:
             if len(companies_pending_status) == 0:
-                return SystemMessages().success('Tarefas do Itau foram executadas com sucesso!')
+                return SystemMessages().success('Todas tarefas do Itau ja foram executadas')
 
         companies_to_execute = companies_pending_status if len(
             companies_from_progress_table) > 0 else ItauConfigs.COMPANIES_TO_EXECUTE
@@ -50,17 +38,19 @@ async def do_itau_tasks():
         companies_itau = CompaniesPage(
             page, companies_to_execute=companies_to_execute)
         accounts = await companies_itau.get_accounts()
-        duckdb_connection.insert_companies_if_not_exists(accounts)
+        progress_repository.insert_companies_if_not_exists(accounts)
 
-        for account in accounts:
+        if len(accounts) == 0:
+            SystemMessages().error(
+                'Nenhuma empresa nos parametros informados foi encontrada, verifique as informações enviadas')
+            return
+
+        async for i, account in tqdm(enumerate(accounts, start=1), total=len(accounts), desc='Contas processadas'):
             try:
                 SystemMessages().log(
                     f'Trocando conta para {account["name"]} - {account["number"]}...')
 
-                if account['index'] == 0:
-                    SystemMessages().log(
-                        f'Conta trocada para {account["name"]} - {account["cnpj"]} - {account["number"]}')
-                else:
+                if account['index'] != 0:
                     await retry.run(companies_itau.change_account, account)
 
                 await retry.run(companies_itau.goto_download_company_page)
@@ -68,17 +58,15 @@ async def do_itau_tasks():
                     page, date_begin=ItauConfigs.DATE_BEGIN, date_end=ItauConfigs.DATE_END, cnpj_company=account['cnpj'])
                 await retry.run(download_itau.search_payments)
 
-                duckdb_connection.update_company_status(account, 'done')
+                progress_repository.update_company_status(account, 'done')
             except:
-                duckdb_connection.update_company_status(account, 'error')
+                progress_repository.update_company_status(account, 'error')
 
-        SystemMessages().success('Tarefas do Itau foram executadas com sucesso!')
+        SystemMessages().success('✅ Tarefas do Itau foram executadas com sucesso!')
 
 
 async def do_netsuite_tasks():
     async with PlaywrightsConfigs() as context:
-        duckdb_connection = await connect_duckdb()
-
         files = get_all_files_in_download_dir()
         if files:
             page = await context.new_page()
@@ -90,21 +78,28 @@ async def do_netsuite_tasks():
             search_netsuite = SearchPage(
                 page, 'https://6391568.app.netsuite.com/app/common/search/search.nl?searchtype=Custom&rectype=286&%E2%80%A6%20NetSuite%20Login')
 
-            for file in files:
+            async for i, file in tqdm(enumerate(files, start=1), total=len(files), desc='Comprovantes processados'):
                 is_supplier_exists = await search_netsuite.search_receipt(file)
 
                 if is_supplier_exists:
                     upload_page = UploadPage(page)
-                    await upload_page.upload_file(file)
+                    await retry.run(upload_page.upload_file, file)
 
                 delete_file_from_path(access_dir('docs\downloads', file))
+        else:
+            SystemMessages().success('Nenhuma tarefa do NetSuite foi encontrada')
+            return
 
-        SystemMessages().success('Tarefas do NetSuite foram executadas com sucesso!')
+        SystemMessages().success('✅ Tarefas do NetSuite foram executadas com sucesso!')
 
-        # if not get_all_files_in_download_dir():
-        #     duckdb_connection.drop_progress_table()
+        if not get_all_files_in_download_dir():
+            progress_repository.drop_progress_table()
 
 if __name__ == '__main__':
     retry = RetryExecuter()
+    connection = DuckConnection(access_dir(
+        'docs\database', 'companies.duckdb')).connect()
+    progress_repository = ProgressRepository(connection)
+
     asyncio.run(retry.run(do_itau_tasks))
     asyncio.run(retry.run(do_netsuite_tasks))
